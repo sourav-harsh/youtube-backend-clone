3c09ffbf1fbbcd967039b154e2e50a1072c25a0c
souravkumar <s4056751@gmail.com>
2026-08-05 13:33:42 +0530

    fix: the user controller if user not provide avatar

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -54,7 +54,7 @@ const registerUser = asyncHandler( async (req, res) => {
     }
     //console.log(req.files);
 
-    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const avatarLocalPath = req.files?.avatar?.at(0)?.path;
     //const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
     let coverImageLocalPath;
